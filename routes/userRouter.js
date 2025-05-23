1a5fe854411c4da6a66abefdcc80e92146b997fb
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-05-24 06:35:56 +0800

    add get request

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,9 @@ const {Router} = require("express");
 const router = Router();
 const userController = require("../controllers/userController.js")
 
+router.get("/", (req, res) => {
+    res.send("Returned user");
+})
 router.post("/", userController.signUpUser);
 
 module.exports = router;
